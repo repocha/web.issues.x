7838915b4698a564496955fecbc6caa0da39e444
Tianyin Xu <xuty1985@gmail.com>
2017-06-30 13:56:30 -0700

    change the interface because we can't understand the complex dir structures

--- crawler.py
+++ crawler.py
@@ -4,76 +4,90 @@ import os
 import logging
 from lxml.html import fromstring
 
-def crawl(url, dstpath, intv=1):
+def __crawl(url, dstpath, intv=1):
   """
   Utility function
   """
   hdr = {'User-Agent': 'Mozilla/5.0'}
   req = urllib2.Request(url,headers=hdr)
-  with open(dstpath, 'w') as of:
-    of.write(urllib2.urlopen(req, timeout=5).read())
+  with open(dstpath, 'w') as f:
+    f.write(urllib2.urlopen(req, timeout=5).read())
     time.sleep(intv)
+
+def __validateHTML(htmlpath):
+  try:
+    with open(htmlpath) as f:
+      fromstring(f.read())
+    return True
+  except:
+    return False
 
 class URLCrawler():
   """
   Given a bunch of URLs, and crawl them one by one
   """
-  def __init__(self, log_file=None):
+  def __init__(self, log_file=None, ishtml=True):
+    self.ishtml = ishtml
     if log_file != None:
       self.log_file = log_file
       logging.basicConfig(filename=log_file,level=logging.DEBUG)
     else:
       logging.basicConfig(level=logging.DEBUG)
 
-  def calibrate(self, urls, download_dir):
+  def calibrate(self, urlpl):
+    """urlpl is a list of (u, f) pairs where u is the url to crawl, and f is the dest
+    file path; we intentionally do not use dict to keep the crawling order.
+    """
     crawled = []
     tocrawl = []
-    for f in os.listdir(download_dir):
-      if os.path.getsize(os.path.join(download_dir, f)) > 0:
-        crawled.append(f) 
-    for url in urls:
-      if self.url2fname(url) not in crawled:
-        tocrawl.append(url)
+    for pair in urlpl:
+      u = pair[0]
+      f = pair[1]
+      if os.path.exists(f) && os.path.getsize(f) > 0 && __validateHTML(f):
+        crawled.append((u, f))
+      else:
+        tocrawl.append((u, f))   
     logging.info('--------------------------------------------------------------------')
     logging.info(' CALIBRATION')
     logging.info('--------------------------------------------------------------------')
-    #print 'URL#1:        ', crawled[0]
-    #print 'URL#2:        ', crawled[1]
-    #print 'URL#3:        ', crawled[2]
-    #print 'URL#4:        ', crawled[3]
-    #print '--------------------------------------------------------------------'
     logging.info('#CRAWLED URLS: ' + str(len(crawled)))
     logging.info('#TOCRAWL URLS: ' + str(len(tocrawl)))
     logging.info('--------------------------------------------------------------------')
     return tocrawl
 
-  def crawl(self, urls, download_dir, mandatory=False):
+  def crawl(self, urlpl, mandatory=False):
+    """urlpl is a list of (u, f) pairs where u is the url to crawl, and f is the dest
+    file path; we intentionally do not use dict to keep the crawling order.
+    """
     scnt = 0
     fcnt = 0
-    if os.path.exists(download_dir) == False:
-      try:
-        os.makedirs(download_dir)
-      except Exception as e:
-        logging.error(e)
-        logging.error('Fail to mkdir: ' + download_dir)
-        return
-    for url in urls:
-      dstpath = os.path.join(download_dir, self.url2fname(url))
-      if mandatory == False and os.path.exists(dstpath):
-        logging.info('SKIP (CRAWLED): ' + url)
+    # generate the dirs first
+    for pair in urlpl:
+      u = pair[0]
+      f = pair[1]
+      d = f[:f.rfind('/')]
+      if os.path.exists(d) == False:
+        try:
+          os.makedirs(d)
+        except Exception as e:
+          logging.error(e)
+          logging.error('Fail to mkdir: ' + download_dir)
+          return
+
+    for pair in urlpl:
+      u = pair[0]
+      f = pair[1]
+      if mandatory == False and os.path.exists(f):
+        logging.info('SKIP (CRAWLED): ' + u)
       else:
         try:
-          crawl(url, dstpath)
-          logging.info('CRAWLED: ' + url)
+          __crawl(u, f)
+          logging.info('CRAWLED: ' + u)
           scnt += 1
         except Exception as e:
-          logging.error('FAILURE: ' + url)
+          logging.error('FAILURE: ' + u)
           logging.error(e)
           fcnt +=1
     logging.info('#Crawled URLs: ' + str(scnt))
     if fcnt > 0:
       logging.info('#Failed URLs: ' + str(fcnt))
-
-  def url2fname(self, url):
-    return url[url.rfind('/') + 1:]
-
