f11cedfa4106ea422da902514e2a64adf24d2302
Tianyin Xu <xuty1985@gmail.com>
2017-07-02 01:01:21 -0700

    new query

--- github.pr/query.py
+++ github.pr/query.py
@@ -1,3 +1,4 @@
+import os
 import utils
 import pr_parser
 import pr_loader
@@ -7,29 +8,58 @@ sys.path.append('..')
 import selector
 from kwfilter import KWFilter
 
-cs = [['check', 'style']]
-#with open('kw.checks.lst') as f:
-#  for l in f:
-#    l = l.strip()
-#    if l.startswith('#') == False:
-#      cs.append(l.split(' '))
-kwfilt = KWFilter(cs)
+def queryall(prs_dir):
+  hit = 0
+  cs = [['check', 'style']]
+  with open('kw.checks.lst') as f:
+    for l in f:
+      l = l.strip()
+      if l.startswith('#') == False:
+        cs.append(l.split(' '))
+  kwfilt = KWFilter(cs)
+  for repo in os.listdir(prs_dir):
+    prs = pr_loader.load_prs(os.path.join(prs_dir, repo))
+    for pr in prs:
+      url = pr[0]
+      cnt = pr[1]
+      try:
+        t = pr_parser.parsePRHTML(cnt, url)
+        if t != None and selector.interested(t, kwfilt):
+          hit += 1
+          print url, t.title
+      except Exception as e:
+        print e
+        continue
+  kwfilt.print_kw_matches()
+  print 'HIT:', hit
 
-hit = 0
-#prs = pr_loader.load_prs('tests_aggr/alibaba.dubbo')
-prs = pr_loader.load_prs('tests_aggr/google.guava')
-for pr in prs:
-  url = pr[0]
-  cnt = pr[1]
-  try:
-    t = pr_parser.parsePRHTML(cnt, url)
-    if t != None and selector.interested(t, kwfilt):
-      hit += 1
-      print url, t.title
-  except Exception as e:
-    print e
-    continue
+def query(prs_dir, repo_name):
+  cs = [['check', 'style']]
+  #with open('kw.checks.lst') as f:
+  #  for l in f:
+  #    l = l.strip()
+  #    if l.startswith('#') == False:
+  #      cs.append(l.split(' '))
+  kwfilt = KWFilter(cs)
+  #prs = pr_loader.load_prs('tests_aggr/alibaba.dubbo')
+  #prs = pr_loader.load_prs('tests_aggr/google.guava')
+  prs = pr_loader.load_prs(os.path.join(prs_dir, repo_name))
+  for pr in prs:
+    url = pr[0]
+    cnt = pr[1]
+    try:
+      t = pr_parser.parsePRHTML(cnt, url)
+      if t != None and selector.interested(t, kwfilt):
+        print url, t.title
+    except Exception as e:
+      print e
+      continue
 
-kwfilt.print_kw_matches()
-
-print 'HIT:', hit
+if __name__ == "__main__":
+  """
+  if len(sys.argv) != 2:
+    print '[usage] python query.py $repo_name'
+  else:
+    query('tests_aggr', sys.argv[1])
+  """
+  queryall('tests_aggr')
